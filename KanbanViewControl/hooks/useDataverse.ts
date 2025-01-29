4244056bcce03aa47b2ca805be5520cb64b8d35d
Mário Rocha <mario@novalogica.pt>
2025-01-29 17:12:13 +0000

    minor fix when fetching business process flows

--- KanbanViewControl/hooks/useDataverse.ts
+++ KanbanViewControl/hooks/useDataverse.ts
@@ -40,7 +40,7 @@ export const useDataverse = (context: ComponentFramework.Context<IInputs>) => {
     const getBusinessProcessFlows = async (logicalName: string, records: string[]) => {
         try {
             const stages = await webAPI.retrieveMultipleRecords(
-                "processstages",
+                "processstage",
                 `?$select=stagename,processstageid,stagecategory,_processid_value&$filter=primaryentitytypecode eq '${logicalName}'&$expand=processid($select=name,uniquename)`
             )
 
