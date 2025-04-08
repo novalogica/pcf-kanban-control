0d0327e5b00fd9010f055dc2c86e50b144148662
ayushnvs <ayush.mishra.ald@gmail.com>
2025-04-08 16:31:59 +0530

    Remove drag and drop functionality from cards

--- KanbanViewControl/components/column/Column.tsx
+++ KanbanViewControl/components/column/Column.tsx
@@ -1,50 +1,24 @@
 import * as React from "react";
-import { Draggable, Droppable } from "@hello-pangea/dnd";
 import Card from "../card/Card";
 import ColumnHeader from "./ColumnHeader";
 import { ColumnItem } from "../../interfaces";
 import { isNullOrEmpty } from "../../lib/utils";
 import NoResults from "../container/no-results";
-import { getItemStyle, getListStyle } from "../../lib/card-drag";
 
-const Column = ({ column } : { column: ColumnItem }) => {
-  return ( 
+const Column = ({ column }: { column: ColumnItem }) => {
+  return (
     <div className="column-container">
       <ColumnHeader column={column} />
-      <Droppable key={column.id.toString()} droppableId={column.id.toString()}>
-        {(provided, snapshot) => (
-            <div 
-              ref={provided.innerRef} 
-              className="cards-wrapper" 
-              style={getListStyle(snapshot.isDraggingOver)}
-              {...provided.droppableProps}
-            >
-              {
-                !isNullOrEmpty(column.cards) && column.cards!.length > 0 && column.cards?.map((item, index) => (
-                  <Draggable
-                    key={item.id}
-                    draggableId={item.id.toString()}
-                    index={index}>
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef} 
-                          {...provided.dragHandleProps}
-                          {...provided.draggableProps}
-                          style={getItemStyle(snapshot, provided.draggableProps.style)}
-                        >
-                          <Card key={item.id} item={item} />
-                        </div>
-                      )}
-                  </Draggable>
-                ))
-              }
-              {
-                isNullOrEmpty(column.cards) || column.cards!.length <= 0 && <NoResults />
-              }
-              {provided.placeholder}
-            </div>
-        )}
-      </Droppable>
+      <div className="cards-wrapper">
+        {
+          !isNullOrEmpty(column.cards) && column.cards!.length > 0 && column.cards?.map((item, index) => (
+            <Card key={item.id} item={item} />
+          ))
+        }
+        {
+          isNullOrEmpty(column.cards) || column.cards!.length <= 0 && <NoResults />
+        }
+      </div>
     </div>
   );
 }
